b6cdc72896a00867d1336bf6aee2cf37ca7cfced
Oleg Grafov <olegraf@gmail.com>
2019-09-30 12:43:00 +0300

    provide file name as argument

--- Program.cs
+++ Program.cs
@@ -14,13 +14,27 @@ namespace Rec
     {
         static void Main(string[] args)
         {
-            string recsFolderPath = @".\Recordings\";
-            if (!Directory.Exists(recsFolderPath))
-                Directory.CreateDirectory(recsFolderPath);
+            string recPath = GetRecordingsPath(@"c:\decisions\Recordings\");
+            string fileName = args.Length > 0 && !String.IsNullOrWhiteSpace(args[0]) ? args[0] + ".wma" : $"{DateTime.Now:yyyy-MMM-dd-hhmm}.wma";
+            string filePath = Path.Combine(recPath, fileName);
 
-            string fileName = Path.Combine(recsFolderPath, $"{DateTime.Now.ToShortDateString()}-{DateTime.Now.ToString("hhmm")}.wma");
-            Console.WriteLine("Starting recording to " + fileName);
-            AudioRecorder.RecordToWma(fileName);
+            Console.WriteLine("Starting recording to " + filePath);
+            AudioRecorder.RecordToWma(filePath);
+        }
+
+        private static string GetRecordingsPath(string suggestedPath)
+        {
+            if (String.IsNullOrEmpty(suggestedPath))
+            {
+                const string defaultRecPath = @".\Recordings\";
+                if (!Directory.Exists(defaultRecPath))
+                    Directory.CreateDirectory(defaultRecPath);
+                return defaultRecPath;
+            }
+
+            if (!Directory.Exists(suggestedPath))
+                Directory.CreateDirectory(suggestedPath);
+            return suggestedPath;
         }
     }
 }
